05acaa4f10a2c29872f13e6f134d1ae3bd7e3514
Mansib Mursalin <mansibm6@gmail.com>
2021-10-03 09:15:39 +0600

    Update imagesegmentation.m

--- imagesegmentation.m
+++ imagesegmentation.m
@@ -3,7 +3,7 @@ img1=rgb2gray(img1);
 imshow(img1)
 hold on
 
-img2=imbinarize(img1,graythresh(img1));
+img2=imbinarize(img1,graythresh(img1)); % graythresh finds a threshold value using Otsu's method and imbinarize uses this value to convert to binary image
 imshow(img2)
 img2=~img2;
 imshow(img2)
